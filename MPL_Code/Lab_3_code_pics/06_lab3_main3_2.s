3ea9ce2508d5207131559f12321f60a42666657e
vava24680 <9203700@gmail.com>
2017-10-17 19:15:21 +0800

    06_lab3_main3_2

--- MPL_Code/Lab_3_code_pics/06_lab3_main3_2.s
+++ MPL_Code/Lab_3_code_pics/06_lab3_main3_2.s
@@ -4,60 +4,63 @@
 .data
 	result: .word 0
 	max_size: .word 0
-	user_stack: .zero 128
+	user_stack: .zero 1024
 .text
 	.global main
 	m: .word 0x5E
 	n: .word 0x60
+
+Program_end:
+	nop
+	b Program_end
+
 main:
 	ldr r0,=m
 	ldr r1,=n
 	ldr r2,[r0]
 	ldr r3,[r1]
-	/*Deal with m==n,m=0,n=0*/
-	/*eors r4,r2,r3//m and n are the same
-		streq r2,[r0]
-		beq Program_end
-	cmp r2,#0
-		streq r3,[r0]
-		beq Program_end
-	cmp r3,#0
-		streq r2,[r0]
-		beq Program_end*/
-	ldr r0,=0//Use to store the GDB result
+
+	ldr r0,=1//Use to store the GDB result
 	ldr r1,=0//Use to record the max size using by stack
 	ldr r10,=user_stack //Stack Limit
-	adds r9,r10,#128 //Stack Base
-	msr MSP,r9
-	subs r6,r9,#4
+	adds r9,r10,#1024 //Stack Base
+	msr msp,r9
+
 	/*----------------------*/
 	push {r2,r3}
 	/*Push neccessary parameter into stack*/
 	bl GCD
 	ldr r4,=max_size
 	ldr r5,=result
-
-
+	str r1,[r4]
+	str r0,[r5]
 	b Program_end
 
+//TODO: Implement your GCD function
 GCD:
-	//pop {r4,r5}
 	ldr r4, [sp]
 	ldr r5, [sp,#4]
 
-	TEQ r4,r5//two operands are the same
-	ITT EQ
-		moveq r0,r4
+
+	cmp r4,r5//two operands are the same
+	ITTTT EQ
+		mrseq r2,msp
+		subeq r1,r9,r2
+		muleq r0,r0,r4
 		bxeq lr
 
 	cmp r4,#0
-	ITT EQ
-		moveq r0,r5
+	ITTTT EQ
+		mrseq r2,msp
+		subeq r1,r9,r2
+		muleq r0,r0,r5
 		bxeq lr
 
 	cmp r5,#0
-	ITT EQ
-		moveq r0,r4
+	ITTTT EQ
+		mrseq r2,msp
+		subeq r1,r9,r2
+		muleq r0,r0,r4
 		bxeq lr
 
 	ands r6,r4,#1
@@ -66,9 +69,10 @@ GCD:
 	orr r8,r8,r7//r8 could be 00,01,10,11
 
 	cmp r8,#0//Both Even
-		ITT EQ
+		ITTT EQ
 			asreq r4,r4,1
 			asreq r5,r5,1
+			lsleq r0,r0,1
 
 	cmp r8,#1//r4->even r5->odd
 		IT EQ
@@ -79,21 +83,29 @@ GCD:
 			asreq r5,r5,1
 
 	cmp r8,#3//Both Odd
-		IET EQ
-			bne GCD_RECURSIVE
-			cmpeq r4,r5
-				ITTEE GE
-					subge r4,r4,r5
-					asrge r4,r4,1
-					sublt r5,r5,r4
-					asrlt r5,r5,1
-GCD_RECURSIVE:
-	push {r4,r5,lr}
+		ITT EQ
+		pusheq {r4,r5,lr}
+		bleq Adjust
+	cmp r8,#3
+		IT EQ
+		popeq {r4,r5,lr}
+
+	mov r2, r4
+	mov r3, r5
+	push {r2,r3,lr}
 	bl GCD
-	pop {r4,r5,lr}
+	pop {r2,r3,lr}
 	bx lr
 
-Program_end:
-	nop
-	b Program_end
-//TODO: Implement your GCD function
+Adjust:
+	ldr r2,[sp]
+	ldr r3,[sp,#4]
+	cmp r2,r3
+	ITTEE GE
+		subge r2,r2,r3
+		asrge r2,1
+		sublt r3,r3,r2
+		asrlt r3,1
+	str r2,[sp]
+	str r3,[sp,#4]
+	bx lr
