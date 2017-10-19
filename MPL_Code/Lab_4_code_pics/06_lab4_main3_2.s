42314816ec7beeb42462dcf3da36dfbd292e1eda
vava24680 <9203700@gmail.com>
2017-10-20 02:43:31 +0800

    Need time-slot adjust

--- MPL_Code/Lab_4_code_pics/06_lab4_main3_2.s
+++ MPL_Code/Lab_4_code_pics/06_lab4_main3_2.s
@@ -2,6 +2,7 @@
 	.cpu cortex-m4
 	.thumb
 .data
+	user_stack: .zero 128
 	leds: .byte 0
 .text
 	.global main
@@ -45,49 +46,109 @@ GPIO_init:
 	orrs r1,r1, #0x0
 	str r1,[r0]
 	bx lr
+/*Global Register Usage
+ *R6:Check Register for Debouncing
+ *R7:Current State of Debounce Finite State Machine
+ *R8:Current State of LED Finite State Machine
+ */
+
+/*LED Finite State Machine
+ *Two State
+ *0=>LED Running State(Default State)
+ *1=>LED Stop State
+ */
+
 main:
 	BL GPIO_init
-	movs r1, #1
-	ldr r0, =leds
-	strb r1, [R0]
-	ldr r2,=GPIOB_ODR
-	ldr r5,=#0x0000FFFF
-	strb r5,[r2]
-	ldr r5,=#0xFFFFFFE7
-	mov r6,#-1 //shift direction, 1=>left, -1=>right
-
+	/*Stack Initilization*/
+		ldr r0, =user_stack
+		add r0,r0,#128
+		msr msp,r0
+	/*-------------------*/
+	/*Finite State Initilization*/
+		ldr r6,=#0xFFFFFFFF
+		mov r7,#0
+		mov r8,#1
+	/*--------------------------*/
+	/*DisplayLED Initilization*/
+		mov r2, #1
+		ldr r0, =leds
+		strb r2, [R0]
+		ldr r1,=GPIOB_ODR
+		ldr r3,=#0x0000FFFF
+		strb r3,[r1]
+		ldr r3,=#0xFFFFFFE7
+		mov r4,#-1 //shift direction, 1=>left, -1=>right
+		push {r0-r4}
+	/*------------------------*/
 Loop:
 	BL DisplayLED
 	BL Delay
 	B Loop
 
 DisplayLED:
-	cmp r1,#4
+	/*Using Current State to Change what LED will do*/
+	cmp r8,#1
+	IT NE
+		bne Delay
+	/*----------------------------------------------*/
+	pop {r0-r4}
+	cmp r2,#4
 		IT EQ
-		rsbeq r6,r6,#0
-	cmp r1,#0
+		rsbeq r4,r4,#0
+	cmp r2,#0
 		IT EQ
-		rsbeq r6,r6,#0
+		rsbeq r4,r4,#0
 
-	cmp r6,#1
+	cmp r4,#1
 	ITE EQ
-		lsleq r5,r5,1
-		asrne r5,r5,1
+		lsleq r3,r3,1
+		asrne r3,r3,1
 
-	and r3,r5,#1
-	cmp r3,#0
-	IT EQ
-		orreq r5,r5,#1
-	strb r5,[r2]
-	add r1,r1,r6
-	str r1,[r0]
+	push {r0}
+		and r0,r3,#1
+		cmp r0,#0
+		IT EQ
+			orreq r3,r3,#1
+	pop {r0}
+
+	strb r3,[r1]
+	add r2,r2,r4
+	str r2,[r0]
+
+	push {r0-r4}
 	bx lr
 
 Delay:
-	ldr r3,=#2000
-	L1: ldr r4,=#610
-	L2: subs r4,#1
-	bne L2
-	subs r3,#1
-	bne L1
+	ldr r1,=#350000
+	ldr r0, =GPIOC_IDR
+	Check:
+	/*Get the input value*/
+		ldr r5, [r0] //2
+		lsr r5,r5,13 //2
+		and r5,r5,#0x00000001 //1
+	/*-------------------*/
+	/*Push the input value into Chech Register(R6)*/
+		lsl r6,r6,1 //1
+		orr r6,r6,r5 //1
+	/*--------------------------------------------*/
+	/*State Changing and Checking*/
+		cmp r7,#1 //1
+		IT EQ
+			addeq r7,r7,r5
+		cmp r7,#2 //1
+		ITTT EQ
+			rsbeq r8,r8,#0
+			moveq r7,#0
+			beq DisplayLED
+		//bne Check
+		cmp r7,#0
+		IT EQ
+			cmpeq r6,#0
+			IT EQ
+				moveq r7,#1
+	sub r1,r1,#1
+	cmp r1,#0
+	bne Check
+	/*---------------------------*/
 	bx lr
