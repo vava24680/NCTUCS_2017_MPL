3a0a7c4c6cdb0c6ed45b934af323cbf1f2511287
vava24680 <9203700@gmail.com>
2017-12-22 13:04:38 +0800

    Test LCD

--- MPL_Code/Lab_9_code_pics/06_lab9_main3_1.c
+++ MPL_Code/Lab_9_code_pics/06_lab9_main3_1.c
@@ -13,6 +13,10 @@ int main(void)
 	uint8_t result;
 	Essential_GPIO_init();
 	lcd_gpio_init();
-	result = BusyCheck();
+	LCD_Function_Set(0x0001U, 0x0001U, 0x0000U);
+	LCD_Enter_Mode(0x0001U, 0x0000U);
+	LCD_Panel_Switcher(0x0001U, 0x0000U, 0x0000U);
+	LCD_ClearScreen();
+	LCD_WriteData((uint16_t)'A');
 	return 0;
 }
