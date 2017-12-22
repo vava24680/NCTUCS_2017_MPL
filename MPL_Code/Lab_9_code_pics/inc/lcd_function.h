d9b63be9bc97f787991658977ad058693680fad4
vava24680 <9203700@gmail.com>
2017-12-22 13:05:21 +0800

    More function declaration

--- MPL_Code/Lab_9_code_pics/inc/lcd_function.h
+++ MPL_Code/Lab_9_code_pics/inc/lcd_function.h
@@ -12,9 +12,15 @@
 #include "ref.h"
 
 void lcd_gpio_init(void);
-void WriteCommand(uint16_t command);
-void WriteData(uint16_t data);
 uint8_t BusyCheck(void);
+void LCD_WriteCommand(uint16_t command);
+void LCD_WriteData(uint16_t data);
 void LCD_ClearScreen(void);
-void LCD_Show_CursorShow_Blink(uint16_t show, uint16_t cursor_show, uint16_t blink);
+void LCD_Cursor_Reset(void);
+void LCD_Enter_Mode(uint16_t Incre_Decre, uint16_t Shift_screen);
+void LCD_Panel_Switcher(uint16_t screen_switch, uint16_t cursor_show, uint16_t cursor_blink);
+void LCD_Cursor_Shift_Set(uint16_t object, uint16_t direction);
+void LCD_Function_Set(uint16_t DL, uint16_t N, uint16_t F);
+void LCD_Set_CRGRAM_Address(uint16_t five_bit_address);
+void LCD_Set_DDRAM_Address(uint16_t six_bit_address);
 #endif
